5233d40788a26d9e92919f4e93f476132ae07774
b87775 <jwoods1@fullsail.edu>
2015-01-28 06:43:15 -0700

    added header

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 (function(){
   $(".jumbotron > h1").hover(function(){
     alert('hello');
   })
 })
-=======
+
 $(document).ready(function(){
 	
 	
 });
->>>>>>> d1b4023587160c6039e457eace04e59073170953
+
