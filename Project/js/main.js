8384b692dd442473d1497ce4123ed7db5d031e66
Jason Woods <jwoods1@fullsail.edu>
2015-01-29 14:44:39 -0700

    added Chart.js and bar and line chart to the canvas with slider

--- Project/js/main.js
+++ Project/js/main.js
@@ -47,5 +47,49 @@ $(document).ready(function(){
     var loc = "#"+ $(this).attr('loc');
     $(loc).css("display", "none");
   })
+
+// Charts'
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var ctx = document.getElementById('barChart').getContext("2d");
+var barChart = new Chart(ctx).Bar(data);
+var ctx2 = document.getElementById('lineChart').getContext("2d");
+var lineChart = new Chart(ctx2).Line(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
