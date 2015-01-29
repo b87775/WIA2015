e7a931cdbcca0ed461f3673d63435a8065706a24
b87775 <jwoods1@fullsail.edu>
2015-01-28 22:31:06 -0700

    added map with images and svg with some jquery animation

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,12 +1,48 @@
 
-(function(){
-  $(".jumbotron > h1").hover(function(){
-    alert('hello');
-  })
-})
-
 $(document).ready(function(){
 	
-	
+	// Jquery Effects Animations
+  
+  $( "#plane" ).click(function() {
+    $( "#planeImg" ).animate({
+      opacity: 0.25,
+      left: "+=50",
+      height: "toggle"
+    }, 5000, function() {
+      $("#planeImg").animate({
+        opacity:1,
+        left:"-=50",
+        height: "toggle"
+      })
+    });
+  });
+
+  $("#airways").click(function(){
+    $("#airImg").animate({
+      height:"200px",
+      width:"300px"
+    },500 , function(){
+        $("#airImg").animate({
+        height:"125px",
+        width:"200px"
+      })
+    });
+  });
+
+  //Map Features
+
+  $("a.dot").hover(function(){
+    //alert($(this).attr('loc'));
+    $("a.dot").removeClass("selected");
+    $(this).addClass("selected");
+    var loc = "#"+ $(this).attr('loc');
+    //alert(loc);
+    $(loc).css("display", "block");
+
+  },function(){
+    $("a.dot").removeClass("selected");
+    var loc = "#"+ $(this).attr('loc');
+    $(loc).css("display", "none");
+  })
 });
 
